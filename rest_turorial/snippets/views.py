11aa1bb7f1fd22e973d0f4d5cbe5df0df3a31990
riba1122 <riba1122@gmail.com>
2017-12-09 23:45:13 +0100

    removed cache from repo

--- rest_turorial/snippets/views.py
+++ rest_turorial/snippets/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 
-# Create your views here.
+@csrf_exempt
+def snippet_list(request):
+    """
+    List all code snippets, or create a new snippet
+    """
