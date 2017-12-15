7b48b261cb464ae3d336213fe6b03e883fef69fb
riba1122 <riba1122@gmail.com>
2017-12-15 15:03:50 +0100

    changed to use class based views

--- rest_turorial/snippets/urls.py
+++ rest_turorial/snippets/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
+# from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 
     # path('snippets/', views.snippet_list),
     # path('snippets/<int:pk>/', views.snippet_detail),
