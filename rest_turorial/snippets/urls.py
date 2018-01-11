c0274550a033e9753a5beff80b31ac597e872392
riba1122 <riba1122@gmail.com>
2018-01-11 14:24:56 +0100

    added names for urlconfs

--- rest_turorial/snippets/urls.py
+++ rest_turorial/snippets/urls.py
@@ -3,19 +3,22 @@ from rest_framework.urlpatterns import format_suffix_patterns
 # from django.urls import path
 from . import views
 
-urlpatterns = [
-    url(r'^snippets/$', views.SnippetList.as_view()),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
-    url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
-    url(r'^api-auth/', include('rest_framework.urls',
-                               namespace='rest_framework')),
+urlpatterns = format_suffix_patterns([
     url(r'^$', views.api_root),
+    url(r'^snippets/$', views.SnippetList.as_view(), name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(),
+        name='snippet-detail'),
     url(r'^snippets/(?P<pk>[0-9]+)/highlights/$',
-        views.SnippetHighlight.as_view()),
+        views.SnippetHighlight.as_view(), name='snippet-highlight'),
+    url(r'^users/$', views.UserList.as_view(), name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(),
+        name='user-detail'),
     # path('snippets/', views.snippet_list),
     # path('snippets/<int:pk>/', views.snippet_detail),
 
-]
+])
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+        namespace='rest_framework')),
+]
